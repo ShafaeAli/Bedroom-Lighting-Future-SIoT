24cd4f3ef4d18e0db1022bf90dc2964ec8a8d6cb
ShafaeAli <33464609+ShafaeAli@users.noreply.github.com>
2021-12-23 09:43:47 +0000

    Update blinds_alexa.py

--- circadian_rhythm/pi4_sunset_blinds/blinds_alexa.py
+++ circadian_rhythm/pi4_sunset_blinds/blinds_alexa.py
@@ -107,39 +107,5 @@ if __name__=='__main__':
     app.run(debug=True,use_reloader=False)
     #p.join()
 
-"""
-
-    
-
-#if the current elevation is less than dawn or dusk, then servo should be drivenn till endstop 1 active
-#if the current elevation is anything else, then servo should be driven till endstop 2 active
-#if the servo has been on for more than x amount of time, shut it down.
-try:
-    while True:
-        current_elevation, dawn_elevation, dusk_elevation = get_elevation()
-        #if GPIO.input(LimitSwitchShut):
-        if current_elevation < dawn_elevation or current_elevation < dusk_aacelevation:
-            print("it is early morning or night ", current_elevation)
-            #if GPIO.input(LimitSwitchShut):
-            pwm.set_servo_pulsewidth( servo, 500 ) ;
-            time.sleep(1)
-            #else:
-            pwm.set_PWM_dutycycle(servo, 0);
-            #pwm.set_PWM_freqduency( servo, 0 );
-        elif current_elevation > dawn_elevation or current_elevation > dusk_elevation:
-            #print("it is daytime ", current_elevation)
-            #if GPIO.input(LimitSwitchOpen):
-            pwm.set_servo_pulsewidth( servo, 2500 ) ;
-            time.sleep(1)
-            #else:
-            pwm.set_PWM_dutycycle(servo, 0);
-            #pwm.set_PWM_frequency( servo, 0 );
-except KeyboardInterrupt:
-    print("interrupted")
-    pwm.set_PWM_dutycycle(servo, 0)
-    pwm.set_PWM_frequency( servo, 0 )
-    #pwm.set_PWM_dutycycle(servo, 0)
-    #pwm.set_PWM_frequency( servo, 0 )"""
-
 
 
